8553675725d9fd06045d468c527fb0b4d9f8a14f
talha <talhasiddique07@gmail>
2024-05-12 19:37:13 +0530

    Add all the prerequisites

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { AppContext } from "./context/contextApi";
 
 const App = () => {
-  return <div className='text-4xl'>App</div>;
+  return (
+     < AppContext>
+        <div className='text-4xl'>App</div>
+     </ AppContext>
+     );
 };
+
 export default App
---
+++ src/context/contextApi.js
@@ -0,0 +1,45 @@
+import React, {createContext, useState, useEffect}  from "react";
+
+import {fetchDataFromApi} from "../utils/api";
+
+export const Context = createContext();
+
+export const AppContext = (props) => {
+    //This will contain all the logic that we want to make available globally
+    //we will create react states here
+    const [loading, setLoading] = useState(false);
+    const [searchResults, setSearchResults] = useState(false);
+    const [selectCategories, setSelectCategories] = useState("New");
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    useEffect(() => {
+       // This will take a dependency array
+       fetchSelectedCategoryData(selectCategories)
+    }, [selectCategories]) // whenever the client selects a category it will kickstart this method and the respected api will be called to fetch the data for that particular category
+    
+    const fetchSelectedCategoryData = (query) => {
+         setLoading(true)
+         fetchDataFromApi(`search/?q=${query}`).then((res) => {
+            console.log(res)
+            //setSearchResults(res)
+            setLoading(false)
+         })   
+    }
+    return (
+        <Context.Provider 
+        value = {{
+            loading, 
+            setLoading,
+            searchResults,
+            setSearchResults,
+            selectCategories,
+            setSelectCategories,
+            mobileMenu,
+            setMobileMenu,
+        }}>         
+            {props.children}
+        </Context.Provider>
+    ) // intention is to pass all the states and methods in the provider such that they are available throughout the website
+}
+
+    
